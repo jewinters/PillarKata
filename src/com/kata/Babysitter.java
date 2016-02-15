0034e639d712ec5b85b3560d4a9292e0e573b4d6
Jeremy Winters <jwinters@2co.com>
2016-02-15 18:00:33 -0500

    Quick refactor

--- src/com/kata/Babysitter.java
+++ src/com/kata/Babysitter.java
@@ -9,15 +9,24 @@ public class Babysitter {
 
     public static int getRate(Calendar startTime, Calendar endTime, Calendar bedTime) throws Exception {
 
+        int startHour, endHour, bedHour;
+
+        //Turn our Calendars into integer-based hours for ease of use.
+        startHour = startTime.get(Calendar.HOUR_OF_DAY);
+        endHour = endTime.get(Calendar.HOUR_OF_DAY);
+        bedHour = bedTime.get(Calendar.HOUR_OF_DAY);
+
         int total = 0;
 
         //Start Time must be after 5:00 PM, but it could also be prior to 4:00 AM.
-        if ( startTime.get(Calendar.HOUR_OF_DAY) < 17 )
+        if ( startHour < 17 )
             throw new Exception("Start time must be after 5:00 PM");
 
-        if ( endTime.get(Calendar.HOUR_OF_DAY) > 4 )
+        //End time must be before 4:00 AM
+        if ( endHour > 4 )
             throw new Exception("End time must be before 4:00 AM");
 
+        //End time must be after start time. This addresses issues where end time is < 5:00 PM or start time > 4:00 AM
         if ( endTime.before(startTime) )
             throw new Exception("End time must be after start time");
 
--- src/com/kata/BabysitterTest.java
+++ src/com/kata/BabysitterTest.java
@@ -48,5 +48,4 @@ public class BabysitterTest extends TestCase {
         }
     }
 
-
 }
