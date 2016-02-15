4bbd5f9af87164bf3f7fca20e7ecc3dee1e954a3
Jeremy Winters <jwinters@2co.com>
2016-02-15 17:45:26 -0500

    First test

--- src/com/kata/Babysitter.java
+++ src/com/kata/Babysitter.java
@@ -1,7 +1,21 @@
 package com.kata;
 
+import java.util.Calendar;
+
 /**
  * Created by jwinters on 2/15/16.
  */
 public class Babysitter {
+
+    public static int getRate(Calendar startTime, Calendar endTime, Calendar bedTime) throws Exception {
+
+        int total = 0;
+
+        //Start Time must be after 5:00 PM, but it could also be prior to 4:00 AM.
+        if ( startTime.get(Calendar.HOUR_OF_DAY) < 17 )
+            throw new Exception("Start time must be after 5:00 PM");
+
+        return total;
+    }
+
 }
--- src/com/kata/BabysitterTest.java
+++ src/com/kata/BabysitterTest.java
@@ -1,7 +1,23 @@
 package com.kata;
 
+import junit.framework.*;
+import java.util.GregorianCalendar;
+
 /**
  * Created by jwinters on 2/15/16.
  */
-public class BabysitterTest {
+public class BabysitterTest extends TestCase {
+
+    public void testStartTimeCannotBeEarlierThanFivePM() {
+        GregorianCalendar startTime = new GregorianCalendar(2016, 0, 1, 16, 0);
+        GregorianCalendar endTime = new GregorianCalendar(2016, 0, 2, 4, 0);
+        GregorianCalendar bedTime = new GregorianCalendar(2016, 0, 2, 0, 0);
+        try {
+            Babysitter.getRate(startTime, endTime, bedTime);
+            fail("Should throw an exception if start time is before 5:00 PM");
+        } catch (Exception e) {
+            assert (e instanceof Exception);
+            assertEquals("Start time must be after 5:00 PM", e.getMessage());
+        }
+    }
 }
