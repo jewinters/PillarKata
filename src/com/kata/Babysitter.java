b92e373ea276eef83955a489f58ad721847e1ffa
jewinters <jewinters@gmail.com>
2017-04-20 12:56:00 -0400

    Code cleanup

--- src/com/kata/Babysitter.java
+++ src/com/kata/Babysitter.java
@@ -1,11 +1,9 @@
 package com.kata;
 
 public class Babysitter {
-
     private static final int PRE_BEDTIME_RATE = 12;
     private static final int POST_BEDTIME_RATE = 8;
     private static final int POST_MIDNIGHT_RATE = 16;
-
     private static final int EARLIEST_START_HOUR = 17;
     private static final int LATEST_END_HOUR = 28;
     private static final int MIDNIGHT = 24;
@@ -16,18 +14,16 @@ public class Babysitter {
         int startHour = normalizeHour(startTime);
         int endHour = normalizeHour(endTime);
         int bedHour = normalizeHour(bedTime);
+        bedHour = bedHour > MIDNIGHT ? MIDNIGHT : bedHour;
 
-        if ( startHour < EARLIEST_START_HOUR || startHour > LATEST_END_HOUR )
+        if (startHour < EARLIEST_START_HOUR || startHour > LATEST_END_HOUR)
             throw new Exception("Start time must be after 5:00 PM and before 4:00 AM");
 
-        if ( startHour >= LATEST_END_HOUR )
+        if (startHour >= LATEST_END_HOUR)
             throw new Exception("Time cannot include midday hours");
 
-        if ( endHour <= startHour || endHour > LATEST_END_HOUR || endHour < EARLIEST_START_HOUR )
+        if (endHour <= startHour || endHour > LATEST_END_HOUR || endHour < EARLIEST_START_HOUR)
             throw new Exception("End time must be before start time and between 5:00 PM and 4:00 AM");
-
-        if ( bedHour > MIDNIGHT )
-            bedHour = MIDNIGHT;
 
         for (int hour = startHour; hour < endHour; hour++)
             total += getHourlyRate(hour, bedHour);
